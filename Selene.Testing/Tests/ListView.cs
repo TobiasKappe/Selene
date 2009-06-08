a952fa669d8575eae6fc2c09ed3304ce753fd390
Tobias Kappé <tobias@ntlabs.org>
2009-06-08 16:53:55 +0200

    Added tests for new flags

--- Selene.Testing/Tests/ListView.cs
+++ Selene.Testing/Tests/ListView.cs
@@ -11,11 +11,26 @@ namespace Selene.Testing
 {
     public partial class Harness
     {
+        static Enclosed Individual = new Enclosed { Name = "Lisa Cuddy", Single = true };
+
         class Container
         {
             [ControlFlags("Person dialog")]
-            public Enclosed[] People = new Enclosed[] {
-                new Enclosed { Name = "Lisa Cuddy", Single = true } };
+            public Enclosed[] People = new Enclosed[] { Individual };
+        }
+
+        // Testing these for all permutations is silly, these two suffice
+        // The adding of buttons is handled by the same function anyway
+        class EditListGrey
+        {
+            [ControlFlags("Person dialog", true, false, false, true)]
+            public Enclosed[] People = new Enclosed[] { Individual };
+        }
+
+        class EditListInv
+        {
+            [ControlFlags("Person dialog", true, false, false, false)]
+            public Enclosed[] People = new Enclosed[] { Individual };
         }
 
         class Enclosed
@@ -29,9 +44,17 @@ namespace Selene.Testing
         {
             var Disp = new NotebookDialog<Container>("Add one person");
             var Test = new Container();
-            
+
             Assert.IsTrue(Disp.Run(Test));
             Assert.AreEqual(Test.People.Length, 2);
+
+            var Disp2 = new NotebookDialog<EditListGrey>("Add/Remove grey");
+            var Test2 = new EditListGrey();
+            Assert.IsTrue(Disp2.Run(Test2));
+
+            var Disp3 = new NotebookDialog<EditListInv>("Invisible add/remove");
+            var Test3 = new EditListInv();
+            Assert.IsTrue(Disp3.Run(Test3));
         }
     }
 }
