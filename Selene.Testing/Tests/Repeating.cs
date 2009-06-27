73845d36ee295233b21788cf48cb8cebf33f24cf
Tobias Kappé <tobias@ntlabs.org>
2009-06-27 19:12:45 +0200

    Added test for re-using a presenter

---
+++ Selene.Testing/Tests/Repeating.cs
@@ -0,0 +1,41 @@
+using System;
+using Selene.Backend;
+using NUnit.Framework;
+
+#if GTK
+using Selene.Gtk.Frontend;
+#endif
+
+#if QYOTO
+using Selene.Qyoto.Frontend;
+#endif
+
+namespace Selene.Testing
+{
+    public partial class Harness
+    {
+        class RepeatTest
+        {
+            public bool Hey;
+            public string Hoi;
+        }
+
+        [Test]
+        public void Repeating()
+        {
+            NotebookDialog<RepeatTest> Repeater = new NotebookDialog<RepeatTest>("First empty");
+
+            var Test1 = new RepeatTest();
+            var Test2 = new RepeatTest();
+
+            Repeater.Run(Test1);
+            Repeater.Run(Test2);
+
+            Assert.IsFalse(Test1.Hey);
+            Assert.IsEmpty(Test1.Hoi);
+
+            Assert.IsTrue(Test2.Hey);
+            Assert.IsNotEmpty(Test2.Hoi);
+        }
+    }
+}
