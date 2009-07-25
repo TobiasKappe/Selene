4ea75fc56878cc536293782b5db4b6cfcf7375d6
Tobias Kappé <tobias@ntlabs.org>
2009-07-25 16:49:01 +0200

    Added support for color picker to Qt, which is now up to par with Gtk =)

---
+++ Selene.Qyoto/Selene.Qyoto.Midend/ColorPicker.cs
@@ -0,0 +1,105 @@
+using System;
+using Qyoto;
+using Selene.Backend;
+
+namespace Selene.Qyoto.Midend
+{
+    internal static class ColorHelper
+    {
+        public static readonly ushort Factor = 256;
+
+        public static ushort MakeUshort(int In)
+        {
+            return (ushort) (In * Factor);
+        }
+
+        public static int MakeInt(ushort In)
+        {
+            return (int) (In/Factor);
+        }
+
+        public static string GetHex(int In)
+        {
+            byte Ret = (byte) In;
+            return Ret.ToString("X").PadLeft(2, '0');
+        }
+
+        public static string HtmlName(QColor In)
+        {
+            return string.Format("#{0}{1}{2}", GetHex(In.Red()), GetHex(In.Green()), GetHex(In.Blue()));
+        }
+
+        public static bool AreSame(QColor A, QColor B)
+        {
+            if(A.Red() != B.Red()) return false;
+            if(A.Green() != B.Green()) return false;
+            if(A.Blue() != B.Blue()) return false;
+            if(A.Alpha() != B.Alpha()) return false;
+
+            return true;
+        }
+    }
+
+    public class ColorPicker : QConverterProxy<ushort[]>
+    {
+        QColor Color;
+
+        protected override ushort[] ActualValue {
+            get {
+                return new ushort[] {
+                    ColorHelper.MakeUshort(Color.Red()),
+                    ColorHelper.MakeUshort(Color.Green()),
+                    ColorHelper.MakeUshort(Color.Green()) };
+            }
+            set {
+                if(value == null)
+                    value = new ushort[] { 0, 0, 0 };
+
+                UpdateColor(new QColor(
+                    ColorHelper.MakeInt(value[0]),
+                    ColorHelper.MakeInt(value[1]),
+                    ColorHelper.MakeInt(value[2])));
+            }
+        }
+
+        void UpdateColor(QColor Update)
+        {
+            Color = Update;
+            QPixmap Map = new QPixmap(16, 16);
+            Map.Fill(Color);
+
+            QPainter Paint = new QPainter(Map);
+            Paint.SetPen(QColor.FromRgb(0,0,0));
+            Paint.DrawRect(0, 0, 15, 15);
+            Paint.End();
+
+            (Widget as QPushButton).icon = new QIcon(Map);
+            (Widget as QPushButton).Text = ColorHelper.HtmlName(Color);
+        }
+
+        protected override Selene.Backend.ControlType[] Supported {
+            get {
+                return new ControlType[] { ControlType.Default, ControlType.Color } ;
+            }
+        }
+
+        protected override QObject Construct ()
+        {
+            QPushButton Ret = new QPushButton();
+            QWidget.Connect(Ret, Qt.SIGNAL("clicked()"), Clicked);
+
+            return Ret;
+        }
+
+        void Clicked()
+        {
+            QColor Temp = QColorDialog.GetColor(Color);
+
+            if(Temp != null && Temp.IsValid() && !ColorHelper.AreSame(Temp, Color))
+            {
+                UpdateColor(Temp);
+                FireChanged();
+            }
+        }
+    }
+}
