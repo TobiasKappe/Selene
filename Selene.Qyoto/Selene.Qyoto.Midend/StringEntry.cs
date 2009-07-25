2bce185d75390a575ab6087cdf18401db5d9fb1f
Tobias Kappé <tobias@ntlabs.org>
2009-07-25 16:48:17 +0200

    Added support for FileSelect and DirectorySelect

--- Selene.Qyoto/Selene.Qyoto.Midend/StringEntry.cs
+++ Selene.Qyoto/Selene.Qyoto.Midend/StringEntry.cs
@@ -6,33 +6,75 @@ namespace Selene.Qyoto.Midend
 {
     public class StringEntry : QConverterProxy<string>
     {
+        string Selected = "";
+
         protected override string ActualValue {
             get
             {
-                return (Widget as QLineEdit).Text;
+                if(Orig.SubType == ControlType.Entry || Orig.SubType == ControlType.Default)
+                    return (Widget as QLineEdit).Text;
+                else return Selected;
             }
             set
             {
-                (Widget as QLineEdit).Text = value;
+                if(Orig.SubType == ControlType.Entry || Orig.SubType == ControlType.Default)
+                    (Widget as QLineEdit).Text = value;
+                else if(Orig.SubType == ControlType.FileSelect || Orig.SubType == ControlType.DirectorySelect)
+                {
+                    if(value != null && value != "")
+                    {
+                        Selected = value;
+                        (Widget as QPushButton).Text = System.IO.Path.GetFileName(Selected);
+                    }
+                }
             }
         }
 
         protected override ControlType[] Supported {
             get
             {
-                // We do not "support" these modes but default to entry for all
                 return new ControlType[] { ControlType.Entry, ControlType.FileSelect, ControlType.DirectorySelect };
             }
         }
 
         protected override QObject Construct ()
         {
-            return new QLineEdit();
+            if(Orig.SubType == ControlType.FileSelect || Orig.SubType == ControlType.DirectorySelect)
+            {
+                QPushButton Button = new QPushButton();
+
+                if(Orig.SubType == ControlType.FileSelect)
+                    Button.Text = "Select file";
+                else Button.Text = "Select directory";
+
+                QWidget.Connect(Button, Qt.SIGNAL("clicked()"), ButtonClicked);
+                // TODO: Get an icon in here as soon as Qt has decent stock handling
+                return Button;
+            }
+            else return new QLineEdit();
         }
 
         protected override string SignalForType (ControlType Type)
         {
-            return "textChanged(QString)";
+            if(Orig.SubType == ControlType.Entry || Orig.SubType == ControlType.Default)
+                return "textChanged(QString)";
+            else return null;
+        }
+
+        void ButtonClicked()
+        {
+            QFileDialog Dialog = new QFileDialog();
+            if(Orig.SubType == ControlType.DirectorySelect)
+                Dialog.fileMode = QFileDialog.FileMode.DirectoryOnly;
+            else if(Orig.SubType == ControlType.FileSelect)
+                Dialog.fileMode = QFileDialog.FileMode.ExistingFile;
+
+            if(Dialog.Exec() != 0)
+            {
+                Selected = Dialog.SelectedFiles()[0];
+                (Widget as QPushButton).Text = System.IO.Path.GetFileName(Selected);
+                FireChanged();
+            }
         }
     }
 }
