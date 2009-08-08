3a4b6f542af219e79b6bcd3d03d0a2bb83fd95a5
Tobias Kappé <tobias@ntlabs.org>
2009-08-08 14:28:56 +0200

    Updated tests to work with FlagsBase

--- Selene.Testing/Tests/Changing.cs
+++ Selene.Testing/Tests/Changing.cs
@@ -14,22 +14,27 @@ namespace Selene.Testing
 {
     public partial class Harness
     {
+        [Flags]
+        enum Fruit { Apple = 1, Orange = 2, Banana = 4 }
+        enum Direction { Left, Right, Back }
+
         class Change
         {
+            // Simple widgets
+            public bool Check;
             public string Enter;
             [Control(Override = ControlType.FileSelect)]
             public string File;
 
-            public bool Check;
+            // Enum-based widgets
+            public Fruit Bowl = Fruit.Orange | Fruit.Apple;
+            [Control(Override = ControlType.Radio)]
+            public Direction Choose;
+            public Direction Choose2;
 
-            public AttributeTargets Toggle;
-            [Control(Override = ControlType.Radio), ControlFlags(true)]
-            public Base64FormattingOptions Choose;
-
+            // Compound widgets
             public DateTime Calendar;
-
             public ushort[] Color = new ushort[] { ushort.MaxValue, 0, 0 };
-
             public Enclosed[] List = new Enclosed[] {};
         }
 
@@ -45,10 +50,10 @@ namespace Selene.Testing
             // We stuplidly assume every widget is changed once
 
 #if QYOTO
-            Assert.GreaterOrEqual(TimesChanged, 8);
+            Assert.GreaterOrEqual(TimesChanged, 9);
 #endif
 #if GTK
-            Assert.GreaterOrEqual(TimesChanged, 13);
+            Assert.GreaterOrEqual(TimesChanged, 15);
 #endif
         }
 
---
+++ Selene.Testing/Tests/Flags.cs
@@ -0,0 +1,34 @@
+using System;
+using Selene.Backend;
+using NUnit.Framework;
+
+#if GTK
+using Selene.Gtk.Frontend;
+#endif
+
+#if QYOTO
+using Selene.Qyoto.Frontend;
+#endif
+
+namespace Selene.Testing
+{
+    public partial class Harness
+    {
+        class FlagsTest
+        {
+            public Fruit Choose;
+        }
+
+        [Test]
+        public void Flags()
+        {
+            var Test = new NotebookDialog<FlagsTest>("Which is good?");
+            FlagsTest Result = new FlagsTest();
+            Test.Run(Result);
+
+            Assert.AreEqual(Result.Choose | Fruit.Apple, Result.Choose);
+            Assert.AreNotEqual(Result.Choose | Fruit.Orange, Result.Choose);
+            Assert.AreEqual(Result.Choose | Fruit.Banana, Result.Choose);
+        }
+    }
+}
