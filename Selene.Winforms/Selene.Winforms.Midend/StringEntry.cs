bcb10f46742541d616c4c4e8f7923e39316a2bd8
Tobias Kappé <tobias@ntlabs.org>
2009-09-20 13:17:39 +0200

    Stubbed winforms frontend (nonfunctional)

---
+++ Selene.Winforms/Selene.Winforms.Midend/StringEntry.cs
@@ -0,0 +1,53 @@
+// Copyright (c) 2009 Tobias Kappé
+// 
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// Except as contained in this notice, the name(s) of the above
+// copyright holders shall not be used in advertising or otherwise
+// to promote the sale, use or other dealings in this Software
+// without prior written authorization.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using Selene.Backend;
+using Forms = System.Windows.Forms;
+using System.Windows.Forms;
+
+namespace Selene.Winforms.Midend
+{
+    public class StringEntry : ConverterBase<Forms.Control, string>
+    {
+        protected override string ActualValue {
+            get { return (Widget as TextBox).Text; }
+            set { (Widget as TextBox).Text = value; }
+        }
+
+        protected override Forms.Control Construct ()
+        {
+            return new TextBox();
+        }
+
+        public event EventHandler Changed {
+            add { (Widget as TextBox).TextChanged += value; }
+            remove { (Widget as TextBox).TextChanged -= value; }
+        }
+    }
+}
