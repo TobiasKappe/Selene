49464baccfceea5968459492d706f8c3dfc25cb3
Tobias Kappé <tobias@ntlabs.org>
2009-09-27 12:17:45 +0200

    Added EnumChooser. Windows frontend passes Simplest test

---
+++ Selene.Winforms/Selene.Winforms.Midend/EnumChooser.cs
@@ -0,0 +1,60 @@
+// Copyright (c) 2009 Tobias Kappé
+// 
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// Except as contained in this notice, the name(s) of the above
+// copyright holders shall not be used in advertising or otherwise
+// to promote the sale, use or other dealings in this Software
+// without prior written authorization.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using Forms = System.Windows.Forms;
+using System.Windows.Forms;
+using Selene.Backend;
+
+namespace Selene.Winforms
+{
+    public class EnumChooser : EnumBase<Forms.Control>
+    {
+        protected override int CurrentIndex {
+            get { return (Widget as ComboBox).SelectedIndex; }
+            set { (Widget as ComboBox).SelectedIndex = value; }
+        }
+
+        protected override Forms.Control Construct ()
+        {
+            ComboBox Ret = new ComboBox();
+            Ret.DropDownStyle = ComboBoxStyle.DropDownList;
+            return Ret;
+        }
+
+        public event EventHandler Changed {
+            add { (Widget as ComboBox).SelectedIndexChanged += value; }
+            remove { (Widget as ComboBox).SelectedIndexChanged -= value; }
+        }
+
+        protected override void AddOption (string Value)
+        {
+            (Widget as ComboBox).Items.Add(Value);
+        }
+    }
+}
