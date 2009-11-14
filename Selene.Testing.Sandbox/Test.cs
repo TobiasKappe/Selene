47780d9848455f4a87e6b16bbf027dc8c54aecb1
Tobias Kappé <tobias@ntlabs.org>
2009-11-14 13:16:18 +0100

    Surpressed compiler warning

--- Selene.Testing.Sandbox/Test.cs
+++ Selene.Testing.Sandbox/Test.cs
@@ -4,6 +4,8 @@ namespace Selene.Testing.Sandbox
 {
     class Test
     {
+        #pragma warning disable 0649
+        
         public bool Toggle;
         public string Entry;
         public ConsoleColor Color;
